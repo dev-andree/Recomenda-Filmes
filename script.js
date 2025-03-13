65806d09be0f842ad6859cddab7bab99704a9e29
dev-andree <andreguilherme1213@gmail.com>
2025-03-13 16:53:28 -0300

    Implementação da busca em array de objetos + ajuste da aparicao da resposta selecionada

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-let respHumor = document.getElementById("respHumor");
-let humorSelecionado = document.getElementById("humorSelecionado").value;
+//Coleta informações do HTML
+let humorDigitadoPeloUsuario = document.getElementById("humorDigitadoPeloUsuario").value;
+let respostaUsuario = document.getElementById("respostaUsuario");
 let filmeSelecionado = document.getElementById("filmeSelecionado");
 
-function insertHumor() {
-  let respUser = respHumor.innerHTML = humorSelecionado
+//Objeto de filmes
+let filmes = [
+  {
+    nome: "O Poderoso Chefão",  
+    genero: "Drama",  
+    ano: 1972,  
+    humorAssociado: "triste",  
+  },
+  {
+    nome: "Forrest Gump",  
+    genero: "Drama",  
+    ano: 1994,  
+    humorAssociado: "feliz", 
+  }
+]
 
-  if(respUser == humorSelecionado){
-    filmeSelecionado.innerHTML = JSON.stringify(filmes, null, 5)
+function insertHumor() {
+  //Aparecer resposta do usuário na tela
+  let respostaUsuarioVariavel = humorDigitadoPeloUsuario
+  respostaUsuario.innerHTML = respostaUsuarioVariavel
+
+  let encontraFilme = filmes.find(filmes => filmes.humorAssociado === humorDigitadoPeloUsuario)
+
+  //Aparecer filme selecionado baseado no humor do usuário
+  if(encontraFilme){
+    filmeSelecionado.innerHTML = `${encontraFilme.nome}`
   }
 };
-
- let filmes = {
-   nome: "O Poderoso Chefão", 
-   genero: "Drama", 
-   ano: 1972,
-   humorAssociado: "reflexivo",
-}
-
